cd006956e8af8dea3a36f331129535ead1a8ce00
Jerry Ma <jerrymax.jm@outlook.com>
2019-06-20 10:05:43 +0800

    第七组-实习内推平台

---
+++ 2019_实习内推平台/Code/Client/App5/Models/CollectionsPageModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5.Models
+{
+    public class CollectionsPageModel
+    {
+        public static List<WorkFile> GetWorkFiles()
+        {
+            var resumes = new List<Resume>();
+            resumes = ResumesDataSource.GetAllResumes().ToList();
+
+
+            var workFiles = new List<WorkFile>();
+            //todo
+            return workFiles;
+        }
+
+    }
+
+    public class WorkFile
+    {
+        public int FileId { get; set; }
+        public string FileOwner { get; set; }
+        public string FileTitle { get; set; }
+        public string FileCover { get; set; }
+    }
+}
